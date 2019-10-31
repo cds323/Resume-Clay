89b2466ede7c5786e1323d414c4efa8ab6ff7f64
Clay Simonsen <cds323@lehigh.edu>
2019-10-31 17:03:17 -0400

    Wrote Resume
    added information

---
+++ resume claysimonsen.R
@@ -0,0 +1,16 @@
+Clay D. Simonsen - Lehigh University Class of 2023
+______________________________________________________________________________
+Professional Summary
+
+Driven, focused finance major with a strong interest in wealth management and commerical real estate.
+
+Education
+Middletown High School South (2015-2019)
+3.89/4 Unweighted GPA
+Class Rank 28/349
+
+Lehigh Univeristy (2019-2023)
+
+Work Experience
+Butch's Lube N' Wash (2016-2018)
+Luigis Pizzeria (2019)
